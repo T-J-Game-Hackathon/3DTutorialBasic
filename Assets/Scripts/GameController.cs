29978be112655ab656f3db9ae837c72f5bd0fdc9
Reo Hakuta <reonaldhkt+webs@gmail.com>
2022-12-18 01:16:21 +0900

    feat: ✨ (Chapter8) クリア時のUIを作った。

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
   public TMPro.TMP_Text scoreLabel;
+  public GameObject winnerUI;
 
   [SerializeField]
   int score = 0;
@@ -12,16 +13,32 @@ public class GameController : MonoBehaviour
   // Start is called before the first frame update
   void Start()
   {
+    winnerUI.SetActive(false);
     UpdateScore();
   }
 
   // Update is called once per frame
-  void Update() { }
+  void Update()
+  {
+    if (Input.GetKey("escape"))
+      Application.Quit();
+
+    UpdateScore();
+  }
 
   public void UpdateScore()
   {
     score = GameObject.FindGameObjectsWithTag("Item").Length;
     Debug.Log("Score: " + score);
     scoreLabel.GetComponent<TMPro.TMP_Text>().text = score.ToString();
+
+    if (score == 0)
+      GameClear();
+  }
+
+  public void GameClear()
+  {
+    // Make winner UI visible
+    winnerUI.SetActive(true);
   }
 }
