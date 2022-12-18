46600cc7862c9d14d2cacb06b593b56dfd2ece95
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-12-18 11:38:10 +0900

    feat: ✨ chapter8まで完成

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,10 +3,18 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
   public UnityEngine.UI.Text scoreLabel;
+  public GameObject winnerLabelObject;
+  public int finishCount = 10;
 
   public void Update()
   {
     var item = GameObject.Find("Item");
-    scoreLabel.text = item.GetComponent<Item>().collidCount.ToString();
+    var count = item.GetComponent<Item>().collidCount;
+    scoreLabel.text = count.ToString();
+
+    if (count == finishCount)
+    {
+      winnerLabelObject.SetActive(true);
+    }
   }
 }
